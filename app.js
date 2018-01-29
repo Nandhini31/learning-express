136cfb9f5ff02ff3fc827781d4de13fffd640208
Nandhini Narasimhan <nandhini.narasimhan@ladbrokescoral.com>
2018-01-29 14:07:40 +0000

    pushed to remote

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 var expressHandlebars = require('express-handlebars')
 const app = express()
+const markdownJson = require('metalsmith-markdown');
+var fs = require('fs');
+var marked = require('marked')
+var path = require ('path');
 
 app.engine('handlebars',expressHandlebars({defaultLayout:'main'}))
 app.set('view engine','handlebars')
-app.get('/test',  (req, res) => {
-    console.log("testing")
-    res.render('test');
+app.set('views', path.join(__dirname, 'views/layouts'));
+
+app.get('/:id', function(req, res) {
+    fs.readFile('content/' + req.params.id + '/index.md', 'utf8', function (err,content) {
+    if (err) {
+        res.status(404).send('Error: 404 , Page not found!');
+    }
+    else {
+        var parsedContent = marked(content)
+        console.log(parsedContent)
+        res.render('main',{body:parsedContent})
+        }
+    });
     });
     
 app.listen(3000,() => console.log('App is listening to port 3000'))
